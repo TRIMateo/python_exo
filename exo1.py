f2e42dcbda9b3ecb36142e832e3210d968c31b5e
TRIPODI Mateo <mateo.tripodi@student.arperuwelz.be>
2021-12-16 09:08:56 +0100

    Exercice corrigé

---
+++ exo1.py
@@ -0,0 +1,27 @@
+# Créé par mateo.tripodi, le 02-12-2021 en Python 3.7
+
+for number in range(1,101) :
+    print(number)
+
+    if(number == 1) :
+        print("Départ")
+
+    if(number == 50) :
+        print("Moitié")
+
+    if(number == 100) :
+        print("Terminé")
+
+    if(number % 3 == 0) :
+        print("fizz")
+
+    if(number % 5 == 0) :
+        print("buzz")
+
+
+
+
+
+
+
+
